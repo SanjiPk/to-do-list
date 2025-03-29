a98d4b97eb529dfb21a0a89c1decae9e2300443f
sanji <zero.0102003@gmail.com>
2025-03-29 15:30:58 +0330

    change the class for new validator.

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,19 +1,31 @@
 package db;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+
 import db.exception.*;
 
 public class Database {
 
     public static ArrayList<Entity> entities = new ArrayList<>();
     private static int index = 1;
+    private static HashMap<Integer, Validator> validators = new HashMap<>();
 
-    private Database(){}
+    private Database() {
+    }
 
     public static void add(Entity e) {
-        e.id = index;
-        entities.add(e.copy());
-        index++;
+        Validator validator = validators.get(e.getEntityCode());
+
+        try {
+            validator.validate(e);
+            e.id = index;
+            entities.add(e.copy());
+            index++;
+        } catch (InvalidEntityException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 
     public static Entity get(int id) {
@@ -33,11 +45,25 @@ public class Database {
     }
 
     public static void update(Entity e) {
-        for (int i = 0; i < entities.size(); i++)
-            if (entities.get(i).id == e.id) {
-                entities.set(i, e.copy());
-                return;
-            }
-        throw new EntityNotFoundException();
+        Validator validator = validators.get(e.getEntityCode());
+
+        try {
+            validator.validate(e);
+            for (int i = 0; i < entities.size(); i++)
+                if (entities.get(i).id == e.id) {
+                    entities.set(i, e.copy());
+                    return;
+                }
+            throw new EntityNotFoundException();
+        } catch (InvalidEntityException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public static void registerValidator(int entityCode, Validator validator) {
+        if (validators.containsKey(entityCode) || validators.containsValue(validator))
+            throw new IllegalArgumentException("The code and validator must be uniq.");
+
+        validators.put(entityCode, validator);
     }
 }
