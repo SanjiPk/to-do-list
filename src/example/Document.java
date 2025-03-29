80b3fe64915b64b309c26f41da6676ba7cade0e6
sanji <zero.0102003@gmail.com>
2025-03-29 16:55:47 +0330

    implement Document.java class.

---
+++ src/example/Document.java
@@ -0,0 +1,52 @@
+package example;
+
+import java.sql.Date;
+
+import db.Entity;
+import db.Trackable;
+
+public class Document extends Entity implements Trackable {
+
+    public String content;
+    private Date creationDate;
+    private Date lastModificationDate;
+    public static final int Document_ENTITY_CODE = 12;
+
+    public Document(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public void setCreationDate(Date date) {
+        creationDate = date;
+    }
+
+    @Override
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    @Override
+    public void setLastModificationDate(Date date) {
+        lastModificationDate = date;
+    }
+
+    @Override
+    public Date getLastModificationDate() {
+        return lastModificationDate;
+    }
+
+    @Override
+    public Entity copy() {
+        Document documentCopy = new Document(content);
+        documentCopy.creationDate = creationDate;
+        documentCopy.lastModificationDate = lastModificationDate;
+
+        return documentCopy;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return Document_ENTITY_CODE;
+    }
+}
